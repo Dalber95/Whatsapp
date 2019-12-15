cb858e17f4ea6e918cd02bd08472a47d51295b52
Kamil <krusiec.kamil82@gmail.com>
2019-12-15 22:59:49 +0100

    Add redirecting to ChatActivity

--- app/src/main/java/pl/krusiec/whatsapp/UserListActivity.java
+++ app/src/main/java/pl/krusiec/whatsapp/UserListActivity.java
@@ -1,17 +1,31 @@
 package pl.krusiec.whatsapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class UserListActivity extends AppCompatActivity {
 
     ArrayList<String> users = new ArrayList<>();
+    ArrayList<String> keys = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +36,46 @@ public class UserListActivity extends AppCompatActivity {
 
         ListView userListView = findViewById(R.id.userListView);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        userListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(UserListActivity.this, ChatActivity.class);
+                intent.putExtra("email", users.get(position));
+                startActivity(intent);
+            }
+        });
+
         users.clear();
 
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, users);
         userListView.setAdapter(arrayAdapter);
+
+        FirebaseDatabase.getInstance().getReference().child("users").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String email = String.valueOf(dataSnapshot.child("email").getValue());
+
+                if (!email.equals(mAuth.getCurrentUser().getEmail())) {
+                    users.add(email);
+                    keys.add(dataSnapshot.getKey());
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+
     }
 }
